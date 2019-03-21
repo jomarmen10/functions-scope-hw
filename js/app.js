f6f5d61be29cb8962ef5312682340b5ba5580cf1
Jomar Mendoza <Omay@Macintosh-2.local>
2019-03-21 15:39:04 -0700

    Commit 1 - Verbal questions

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 // Within a function, what is the difference between return and console.log?
 // What are the implications of the ability of a function to return a value?
 
-// Answers
+// Answers//
 // parameter is the place holder for argument in a function.
 //
 // console.log("only prints in console it doesnt give you any value")
