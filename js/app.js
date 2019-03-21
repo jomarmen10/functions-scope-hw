b09dfb845e56b1a5f55b25b1cf36ef6a9f5524ee
Jomar Mendoza <Omay@Macintosh-2.local>
2019-03-21 15:34:58 -0700

    basic setup and verbal questions

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// 1. Verbal questions
+// Write answers to the following questions as comments.
+//
+// What is the difference between a parameter and an argument?
+// Within a function, what is the difference between return and console.log?
+// What are the implications of the ability of a function to return a value?
+
+// Answers
+// parameter is the place holder for argument in a function.
+//
+// console.log("only prints in console it doesnt give you any value")
+// return spits out the value of the function
+//
+// it gives you a value after you run the function
